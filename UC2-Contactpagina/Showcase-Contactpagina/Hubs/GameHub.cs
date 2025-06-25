626e409ffceade6b9b31ab03c78d6916499c225a
ColinvD <vanDongen.colin@gmail.com>
2025-06-25 12:42:40 +0200

    Making multiplayer work with phaser objects

--- UC2-Contactpagina/Showcase-Contactpagina/Hubs/GameHub.cs
+++ UC2-Contactpagina/Showcase-Contactpagina/Hubs/GameHub.cs
@@ -1,10 +1,31 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using System.Collections.Concurrent;
 using System.Threading.Tasks;
 
 namespace Showcase_Contactpagina.Hubs
 {
     public class GameHub : Hub
     {
+        private static ConcurrentDictionary<string, string> ConnectedUsers = new();
+
+        public override async Task OnConnectedAsync()
+        {
+            var username = Context.GetHttpContext().Request.Query["username"].ToString();
+            ConnectedUsers[Context.ConnectionId] = username;
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            if (ConnectedUsers.TryRemove(Context.ConnectionId, out var username))
+            {
+                // Informeer alle andere clients dat deze speler vertrokken is
+                await Clients.Others.SendAsync("RemovePlayer", username);
+
+            }
+
+            await base.OnDisconnectedAsync(exception);
+        }
         public async Task UpdatePosition(string username, float x, float y)
         {
             await Clients.Others.SendAsync("ReceivePlayerPosition", username, x, y);
--- UC2-Contactpagina/Showcase-Contactpagina/wwwroot/js/Racinggame.js
+++ UC2-Contactpagina/Showcase-Contactpagina/wwwroot/js/Racinggame.js
@@ -1,81 +1,42 @@
-﻿//const myCanvas = document.getElementById("gameCanvas");
-//const ctx = canvas.getContext("2d");
+﻿const connection = new signalR.HubConnectionBuilder()
+    .withUrl("/gamehub?username=" + encodeURIComponent(username))
+    .build();
 
-//const connection = new signalR.HubConnectionBuilder()
-//    .withUrl("/gamehub")
-//    .build();
-
-//const otherPlayers = {}; // { username: {x, y} }
-
+const otherPlayers = {}; // { username: {x, y} }
 
 //const player = {
 //    x: 400,
-//    y: 300,
-//    width: 40,
-//    height: 40,
-//    speed: 4,
-//    color: "#00ff00",
+//    y: 400,
 //};
 
-//const keys = {};
+connection.start().then(() => {
+    console.log("SignalR connected");
+}).catch(err => console.error(err));
 
-//window.addEventListener("keydown", (e) => keys[e.key] = true);
-//window.addEventListener("keyup", (e) => keys[e.key] = false);
+connection.on("ReceivePlayerPosition", (otherUsername, x, y) => {
+    if (otherUsername !== username) {
+        if (!otherPlayers[otherUsername]) {
+            const sprite = game.scene.keys['GameScene'].physics.add.sprite(x, y, 'car').setScale(0.1);
+            sprite.setCollideWorldBounds(true);
+            otherPlayers[otherUsername] = { sprite };
+        }
+        otherPlayers[otherUsername].sprite.setPosition(x,y);
+    }
+});
 
-//connection.start().then(() => {
-//    console.log("SignalR connected");
-//}).catch(err => console.error(err));
+connection.on("RemovePlayer", (otherUsername) => {
+    if (otherPlayers[otherUsername]) {
+        otherPlayers[otherUsername].sprite.destroy();
+        delete otherPlayers[otherUsername];
+    }
+});
 
-//connection.on("ReceivePlayerPosition", (otherUsername, x, y) => {
-//    if (otherUsername !== username) {
-//        otherPlayers[otherUsername] = { x, y };
-//    }
-//});
+function sendPosition(playerX, playerY) {
+    connection.invoke("UpdatePosition", username, playerX, playerY)
+        .catch(err => console.error(err));
+}
 
-//function sendPosition() {
-//    connection.invoke("UpdatePosition", username, player.x, player.y)
-//        .catch(err => console.error(err));
-//}
-
-//let frameCount = 0;
-
-//function update() {
-//    if (keys["ArrowUp"]) player.y -= player.speed;
-//    if (keys["ArrowDown"]) player.y += player.speed;
-//    if (keys["ArrowLeft"]) player.x -= player.speed;
-//    if (keys["ArrowRight"]) player.x += player.speed;
-
-//    // Niet elke frame zenden om overbelasting te voorkomen
-//    if (++frameCount % 5 === 0) {
-//        sendPosition();
-//    }
-//}
-//function draw() {
-//    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-//    // Eigen speler
-//    ctx.fillStyle = player.color;
-//    ctx.fillRect(player.x, player.y, player.width, player.height);
-//    ctx.fillStyle = "#fff";
-//    ctx.font = "14px Arial";
-//    ctx.fillText(username, player.x, player.y - 10);
-
-//    // Andere spelers
-//    for (const [name, pos] of Object.entries(otherPlayers)) {
-//        ctx.fillStyle = "#ff0000";
-//        ctx.fillRect(pos.x, pos.y, player.width, player.height);
-//        ctx.fillStyle = "#fff";
-//        ctx.fillText(name, pos.x, pos.y - 10);
-//    }
-//}
-
-//function gameLoop() {
-//    update();
-//    draw();
-//    requestAnimationFrame(gameLoop);
-//}
-
-//gameLoop();
+let frameCount = 0;
 class GameScene extends Phaser.Scene {
     constructor() {
         super({ key: 'GameScene' });
@@ -125,6 +86,10 @@ class GameScene extends Phaser.Scene {
             this.myPlayer.setVelocityY(-160);
         } else {
             this.myPlayer.setVelocityY(0);
+        }
+
+        if (++frameCount % 5 === 0) {
+            sendPosition(this.myPlayer.x, this.myPlayer.y);
         }
     }
 }
